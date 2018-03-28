cbc491337ab7b4af0c0483e6e6a17fe8390a5c0c
barbara.prusiewicz@gmail.com prusiewicz <barbara.prusiewicz@gmail.com>
2018-03-28 23:04:28 +0000

    automatically backed up by learn

--- lib/cruise_ship.rb
+++ lib/cruise_ship.rb
@@ -7,6 +7,9 @@
 # }
 
 def select_winner(passengers)
+  # note: I'm not sure this doesn't seem like best example of when to use .each
+  # Since keys are unique, don't we just need to check key? suite_a and if 
+  # defined, test if passenger name starts with 'a'?
   winner = ""
   passengers.each do |suite, name|
     if suite == :suite_a && name.start_with?("A")
